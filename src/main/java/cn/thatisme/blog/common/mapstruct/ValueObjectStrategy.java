bc5ee0555b3e15cf8917fc975249cd1904e338cb
wujinhang <2014848963@qq.com>
2024-03-11 17:33:07 +0800

    mapstruct 处理 ValueObject

--- src/main/java/cn/thatisme/blog/common/mapstruct/ValueObjectStrategy.java
+++ src/main/java/cn/thatisme/blog/common/mapstruct/ValueObjectStrategy.java
@@ -1,9 +1,11 @@
 package cn.thatisme.blog.common.mapstruct;
 
 import cn.thatisme.blog.common.domain.ValueObject;
+import org.mapstruct.TargetType;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * <p></p>
@@ -14,14 +16,17 @@ public class ValueObjectStrategy {
 
     public <T extends ValueObject<T>> String valueObjectToString(T value) {
         if (value instanceof Record) {
-            Field[] fields = value.getClass().getFields();
+            Field[] fields = value.getClass().getDeclaredFields();
             if (fields.length == 0) {
                 return null;
-            } else if (fields.length > 1) {
+            } else {
+                boolean accessible = fields[0].canAccess(value);
+                // 设置对象的访问权限，保证对private的属性的访
+                fields[0].setAccessible(true);
                 try {
-                    Object result = fields[0].get(value);
-                    return result.toString();
+                    return fields[0].get(value).toString();
                 } catch (IllegalAccessException e) {
+                    fields[0].setAccessible(accessible);
                     throw new RuntimeException(e);
                 }
             }
@@ -29,4 +34,11 @@ public class ValueObjectStrategy {
         return null;
     }
 
+    public <T extends ValueObject<T>> T valueObjectToString(String value, @TargetType Class<T> entityClass) {
+        try {
+            return value != null ? entityClass.getDeclaredConstructor(String.class).newInstance(value) : null;
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            return null;
+        }
+    }
 }
--- src/main/java/cn/thatisme/blog/context/infrastructure/converter/LabelPoConverter.java
+++ src/main/java/cn/thatisme/blog/context/infrastructure/converter/LabelPoConverter.java
@@ -1,20 +1,18 @@
 package cn.thatisme.blog.context.infrastructure.converter;
 
+import cn.thatisme.blog.common.mapstruct.ValueObjectStrategy;
 import cn.thatisme.blog.context.domain.label.Label;
 import cn.thatisme.blog.context.infrastructure.persistence.po.LabelPo;
 import jakarta.annotation.Nonnull;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.springframework.core.convert.converter.Converter;
 
 /**
  * <p></p>
  * @author wujinhang 2023/8/18
  */
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = ValueObjectStrategy.class)
 public interface LabelPoConverter extends Converter<LabelPo, Label> {
 
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "name", target = "name.name")
     Label convert(@Nonnull LabelPo po);
 }
